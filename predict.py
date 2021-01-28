2847cced0bf70beab6d72d6c031f8824e32b2431
Monkeydion <75160324+Monkeydion@users.noreply.github.com>
2021-01-28 18:27:29 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import time
 
 # -----------------------------------------------------------------------------
 # Dimensions of images
@@ -13,9 +14,9 @@ input_shape = (img_width, img_height, 3)
 # -----------------------------------------------------------------------------
 # Load model
 # -----------------------------------------------------------------------------
-test_model = load_model('models/VGG16/10.h5')
+test_model = load_model('models/VGG16/1.h5')
 #test_model.load_weights('weights.h5')
-test_model.summary()
+#test_model.summary()
 
 # -----------------------------------------------------------------------------
 # Image data path
@@ -37,7 +38,11 @@ test_generator = test_datagen.flow_from_directory(
     shuffle=False,
     batch_size=batch_size)
 
+start=time.time()
 Y_pred = test_model.predict_generator(test_generator,num_of_test_samples // batch_size)
+end=time.time()
+duration=end-start
+print("Predict time: ",duration)
 y_pred = np.argmax(Y_pred, axis=1)
 print(y_pred)
 
